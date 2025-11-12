b83c12d600dd48e5d754a36b8986841b83661b03
Collins Kibet <kollcibe05@gmail.com>
2025-11-12 22:03:14 +0300

    unsolved

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    list_to_store_values = list()
+    for n in range(length):
+        if len(list_to_store_values) == length:
+            print(n)
+            p = n + 1
+            sum = p + n 
+            print(p)
     pass
+
+print_fibonacci(9)    
