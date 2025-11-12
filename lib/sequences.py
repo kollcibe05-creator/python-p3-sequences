c18d46df541c5319d2d6e05d7d67f4e4a9decaf1
kollcibe-05-creator <kollcibe05@gmail.com>
2025-11-12 22:32:10 +0300

    Refactored print_fibonacci function
    Refactor print_fibonacci to return Fibonacci sequence.

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    list_to_store_values = list()
-    for n in range(length):
-        if len(list_to_store_values) == length:
-            print(n)
-            p = n + 1
-            sum = p + n 
-            print(p)
-    pass
+    if length <= 0:
+        return []
+    elif length == 1:
+        return [1]
+    else:
+        fib_list = [0, 1] 
+        while len(fib_list) < length:
+            next_in_list = fib_list[-1] + fib_list[-2]
+            fib_list.append(next_in_list)       
+        return fib_list
 
 print_fibonacci(9)    
